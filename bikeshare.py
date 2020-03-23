1f30e25b910ae88b5f045673cf2af8c3b85bd4ea
ruyi_tsai <ruyi_tsai@pixart.com>
2020-03-23 12:27:51 +0800

    modify bikeshare.py and README.md

--- bikeshare.py
+++ bikeshare.py
@@ -179,7 +179,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
+	print('done')
 
 if __name__ == "__main__":
 	main()
